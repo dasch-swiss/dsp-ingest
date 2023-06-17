ebe8990feb496140266a6a4e4bef2be7b1feb807
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2023-06-17 12:28:41 +0200

    try without sbt cache

--- src/test/scala/swiss/dasch/api/ImportEndpointSpec.scala
+++ src/test/scala/swiss/dasch/api/ImportEndpointSpec.scala
@@ -9,8 +9,8 @@ import swiss.dasch.api.ImportEndpointSpec.postImport
 import swiss.dasch.config.Configuration.StorageConfig
 import zio.test.{ ZIOSpecDefault, assertCompletes, assertTrue }
 import swiss.dasch.domain.{ AssetService, AssetServiceLive, ProjectShortcode }
-import swiss.dasch.test.SpecConfigurations
-import swiss.dasch.test.SpecConstants.existingProject
+import swiss.dasch.test.{ SpecConfigurations, SpecConstants }
+import swiss.dasch.test.SpecConstants.{ emptyProject, existingProject, nonExistentProject }
 import swiss.dasch.test.SpecFileUtil.pathFromResource
 import zio.{ UIO, URIO, ZIO }
 import zio.http.*
@@ -18,6 +18,7 @@ import zio.nio.file.Files
 object ImportEndpointSpec extends ZIOSpecDefault {
 
   private val validContentTypeHeaders = Headers(Header.ContentType(MediaType.application.zip))
+  private val bodyFromZipFile         = Body.fromFile(pathFromResource("/test-import.zip").toFile)
 
   private def postImport(
       shortcode: String | ProjectShortcode,
@@ -31,43 +32,38 @@ object ImportEndpointSpec extends ZIOSpecDefault {
   val spec = suite("ImportEndpoint")(
     suite("POST on /project/{shortcode}/import should")(
       test("given the shortcode is invalid, return 400")(for {
-        response <- postImport("invalid-shortcode", Body.empty, validContentTypeHeaders)
+        response <- postImport("invalid-shortcode", bodyFromZipFile, validContentTypeHeaders)
       } yield assertTrue(response.status == Status.BadRequest)),
       test("given the Content-Type header is invalid/not-present, return 400")(
         for {
-          responseNoHeader    <- postImport(existingProject, Body.empty, Headers.empty)
+          responseNoHeader    <- postImport(existingProject, bodyFromZipFile, Headers.empty)
           responseWrongHeader <-
-            postImport(existingProject, Body.empty, Headers(Header.ContentType(MediaType.application.json)))
+            postImport(emptyProject, bodyFromZipFile, Headers(Header.ContentType(MediaType.application.json)))
         } yield assertTrue(
           responseNoHeader.status == Status.BadRequest,
           responseWrongHeader.status == Status.BadRequest,
         )
       ),
       test("given the Body is empty, return 400")(for {
-        response <- postImport("0003", body = Body.empty, headers = validContentTypeHeaders)
+        response <- postImport(emptyProject, bodyFromZipFile, validContentTypeHeaders)
       } yield assertTrue(response.status == Status.BadRequest)),
       test("given the Body is a zip, return 200")(
         for {
           storageConfig <- ZIO.service[StorageConfig]
           response      <- postImport(
-                             "0003",
-                             body = Body.fromFile(pathFromResource("/test-import.zip").toFile),
-                             headers = validContentTypeHeaders,
+                             emptyProject,
+                             bodyFromZipFile,
+                             validContentTypeHeaders,
                            )
           importExists  <- Files.isDirectory(storageConfig.assetPath / "0003")
                            && Files.isDirectory(storageConfig.assetPath / "0003" / "fg")
         } yield assertTrue(response.status == Status.Ok, importExists)
       ),
       test("given the Body is not a zip, will return 400") {
-        val shortcode = ProjectShortcode.make("0004").toOption.get
         for {
           storageConfig      <- ZIO.service[StorageConfig]
-          response           <- postImport(
-                                  shortcode.toString,
-                                  body = Body.fromFile(pathFromResource("/test-import-invalid.zip.txt").toFile),
-                                  headers = validContentTypeHeaders,
-                                )
-          importDoesNotExist <- validateImportedProjectExists(storageConfig, shortcode).map(!_)
+          response           <- postImport(emptyProject, bodyFromZipFile, validContentTypeHeaders)
+          importDoesNotExist <- validateImportedProjectExists(storageConfig, emptyProject).map(!_)
         } yield assertTrue(response.status == Status.BadRequest, importDoesNotExist)
       },
     )
