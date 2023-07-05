e3bc2ee9e53f40ca0d907b0c93bf0b847c1d07d9
Marcin Procyk <marcin.procyk@dasch.swiss>
2023-07-05 15:58:54 +0200

    fix compare command

--- src/main/scala/swiss/dasch/domain/SipiRunner.scala
+++ src/main/scala/swiss/dasch/domain/SipiRunner.scala
@@ -1,3 +1,8 @@
+/*
+ * Copyright © 2021 - 2023 Swiss National Data and Service Center for the Humanities and/or DaSCH Service Platform contributors.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package swiss.dasch.domain
 
 import java.io.File
@@ -6,7 +11,8 @@ import zio.ZLayer
 import zio.ZIO
 import swiss.dasch.config.*
 import zio.ZIOAppDefault
-import swiss.dasch.config.Configuration.SipiConfig
+import swiss.dasch.config.Configuration.{SipiConfig, StorageConfig}
+import zio.nio.file.Path
 
 trait SipiCommand {
   def help(): String
@@ -15,7 +21,8 @@ trait SipiCommand {
 
 case class SipiLocalDev() extends SipiCommand {
   private val runSipi                = SipiInContainer()
-  private val dockerImageCmd: String = "docker run daschswiss/knora-sipi:latest -exec"
+// sipi container needs to be started first up docker-compose
+  private val dockerImageCmd: String = "docker exec dsp-ingest-sipi-1 /sipi/sipi"
 
   override def help(): String                                = s"$dockerImageCmd ${runSipi.help()}"
   override def compare(file1: String, file2: String): String = s"$dockerImageCmd ${runSipi.compare(file1, file2)}"
@@ -23,9 +30,10 @@ case class SipiLocalDev() extends SipiCommand {
 
 case class SipiInContainer() extends SipiCommand {
   private val sipiPath: String = "/sipi/sipi"
+  private val imagesPath: String = "/opt/images/"
 
   override def help(): String                                = s"$sipiPath --help"
-  override def compare(file1: String, file2: String): String = s"$sipiPath --Compare $file1 --Compare $file2"
+  override def compare(file1: String, file2: String): String = s"$sipiPath --compare $imagesPath$file1 $imagesPath$file2"
 }
 
 object SipiCommand {
@@ -55,7 +63,7 @@ object ImageHandler extends ZIOAppDefault {
   val run =
     ZIO
       .service[ImageHandler]
-      .tap(runSipi => zio.Console.printLine(runSipi.help()))
-      // .tap(runSipi => println(runSipi.compare()))
+//      .tap(runSipi => zio.Console.printLine(runSipi.help()))
+       .tap(runSipi => zio.Console.printLine(runSipi.compare("pp.jpg", "pp.jpg")))
       .provide(SipiCommand.layer, Configuration.layer, ImageHandlerLive.layer)
 }
