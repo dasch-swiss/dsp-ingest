713f2cb88b507153e3ab649c128cb261f026a3f6
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2023-09-12 11:49:21 +0200

    fmt

--- src/main/scala/swiss/dasch/domain/MaintenanceActions.scala
+++ src/main/scala/swiss/dasch/domain/MaintenanceActions.scala
@@ -102,9 +102,7 @@ object MaintenanceActions {
         _    <- Files.deleteIfExists(infoFilePath) *> Files.createFile(infoFilePath)
         _    <- Files.writeBytes(infoFilePath, Chunk.fromArray(info.toJsonPretty.getBytes))
       } yield 1)
-      .someOrElseZIO(
-        ZIO.logWarning(s"Sipi did not create an original for $c") *> ZIO.succeed(0)
-      )
+      .someOrElseZIO(ZIO.logWarning(s"Sipi did not create an original for $c") *> ZIO.succeed(0))
   }
 
   private def createNewAssetInfoFileContent(c: CreateOriginalFor): IO[Throwable, AssetInfoFileContent] =
