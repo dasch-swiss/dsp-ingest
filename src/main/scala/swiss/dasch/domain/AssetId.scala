fb9d4eb0a1fd4e9144a06152d8ced13049158ef1
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2023-09-12 15:18:22 +0200

    Add AssetId.makeNew

--- src/main/scala/swiss/dasch/domain/AssetId.scala
+++ src/main/scala/swiss/dasch/domain/AssetId.scala
@@ -8,11 +8,21 @@ package swiss.dasch.domain
 import eu.timepit.refined.api.Refined
 import eu.timepit.refined.refineV
 import eu.timepit.refined.string.MatchesRegex
+import swiss.dasch.infrastructure.Base62
+import zio.{ Random, UIO }
 import zio.nio.file.Path
 
 opaque type AssetId = String Refined MatchesRegex["^[a-zA-Z0-9-_]{4,}$"]
+
 object AssetId {
   def make(id: String): Either[String, AssetId] = refineV(id)
+
+  def makeNew: UIO[AssetId] = Random
+    .nextUUID
+    .map(uuid =>
+      // the unsafeApply is safe here because the [[Base62EncodedUuid]] is valid subset of AssetId
+      Refined.unsafeApply(Base62.encode(uuid).value)
+    )
 
   def makeFromPath(file: Path): Option[AssetId] = {
     val filename = file.filename.toString
@@ -21,6 +31,9 @@ object AssetId {
       case false => None
     }
   }
+
+//  def makeNew: UIO[AssetId] = ZIO.succeed(Base62.encode(UUID.randomUUID())).map(AssetId.make)
+
 }
 
 final case class Asset(id: AssetId, belongsToProject: ProjectShortcode)
