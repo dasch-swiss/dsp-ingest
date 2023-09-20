23a734cca84a465b992ea8685613c002c96f91fa
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2023-09-20 15:39:20 +0200

    !fixup

--- src/test/scala/swiss/dasch/api/ProjectsEndpointSpec.scala
+++ src/test/scala/swiss/dasch/api/ProjectsEndpointSpec.scala
@@ -5,14 +5,15 @@
 
 package swiss.dasch.api
 
-import sttp.tapir.server.ziohttp.{ ZioHttpInterpreter, ZioHttpServerOptions }
-import swiss.dasch.api.tapir.{ BaseEndpoints, ProjectResponse, ProjectsEndpoints, ProjectsEndpointsHandler }
+import sttp.tapir.server.ziohttp.{ZioHttpInterpreter, ZioHttpServerOptions}
+import swiss.dasch.api.tapir.ProjectsEndpointsResponses.ProjectResponse
+import swiss.dasch.api.tapir.{BaseEndpoints, ProjectsEndpoints, ProjectsEndpointsHandler}
 import swiss.dasch.domain.*
 import swiss.dasch.test.SpecConfigurations
-import zio.http.{ Request, Root, Status, URL }
+import zio.http.{Request, Root, Status, URL}
 import zio.json.*
-import zio.test.{ ZIOSpecDefault, assertTrue }
-import zio.{ Chunk, ZIO, http }
+import zio.test.{ZIOSpecDefault, assertTrue}
+import zio.{Chunk, ZIO, http}
 
 object ProjectsEndpointSpec extends ZIOSpecDefault {
 
