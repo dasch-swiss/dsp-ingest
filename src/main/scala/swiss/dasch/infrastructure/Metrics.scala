89a18e5f63688597cf38ca29a2e9f0cc3e4adf3d
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2023-09-20 14:58:19 +0200

    organise imports

--- src/main/scala/swiss/dasch/infrastructure/Metrics.scala
+++ src/main/scala/swiss/dasch/infrastructure/Metrics.scala
@@ -5,11 +5,10 @@
 
 package swiss.dasch.infrastructure
 
-import zio.ZLayer
+import zio.{ durationInt, ZLayer }
 import zio.metrics.connectors.prometheus.PrometheusPublisher
 import zio.metrics.connectors.{ MetricsConfig, prometheus }
 import zio.metrics.jvm.DefaultJvmMetrics
-import zio.durationInt
 
 object Metrics {
   val layer: ZLayer[Any, Nothing, PrometheusPublisher] =
