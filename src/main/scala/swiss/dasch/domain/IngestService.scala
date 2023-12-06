5d7a179f92ace920ae7bcda81a64375888c46e9c
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2023-12-06 13:31:14 +0100

    cleanup

--- src/main/scala/swiss/dasch/domain/IngestService.scala
+++ src/main/scala/swiss/dasch/domain/IngestService.scala
@@ -12,51 +12,49 @@ import swiss.dasch.domain.PathOps.fileExtension
 import zio.nio.file.Path
 import zio.{IO, Task, ZIO, ZLayer}
 
-import java.io.{FileNotFoundException, IOException}
+import java.io.IOException
 
-final case class IngestedFile()
 final case class IngestService(
   storage: StorageService,
   imageService: ImageService,
   sipiClient: SipiClient,
   assetInfo: AssetInfoService
 ) {
-  def ingestFile(
-    fileToIngest: Path,
-    project: ProjectShortcode
-  ): Task[Asset] =
+
+  def ingestFile(fileToIngest: Path, project: ProjectShortcode): Task[Asset] =
     for {
-      _        <- ZIO.logInfo(s"Ingesting file $fileToIngest")
-      assetRef <- AssetRef.makeNew(project)
-      _ <- ZIO.whenZIO(FileFilters.isNonHiddenRegularFile(fileToIngest).negate)(
-             ZIO.fail(new FileNotFoundException(s"File $fileToIngest is not a regular file"))
+      _ <- ZIO.logInfo(s"Ingesting file $fileToIngest")
+      _ <- ZIO.whenZIO(FileFilters.isSupported(fileToIngest).negate)(
+             ZIO.fail(new IllegalArgumentException(s"File $fileToIngest is not a supported file."))
            )
-      _        <- storage.getAssetDirectory(assetRef).tap(storage.createDirectories(_))
-      original <- createOriginalFileInAssetDir(fileToIngest, assetRef)
+      assetRef <- AssetRef.makeNew(project)
+      assetDir <- ensureAssetDirectoryExists(assetRef)
+      original <- createOriginalFileInAssetDir(fileToIngest, assetRef, assetDir)
       asset <- ZIO
                  .fromOption(SupportedFileType.fromPath(fileToIngest))
                  .orElseFail(new IllegalArgumentException("Unsupported file type."))
-                 .flatMap { it =>
-                   it match {
-                     case SupportedFileType.StillImage => handleImageFile(original, assetRef)
-                     case SupportedFileType.Other      => handleOtherFile(original, assetRef)
-                     case SupportedFileType.MovingImage =>
-                       ZIO.fail(new NotImplementedError("Video files are not supported yet."))
-                   }
+                 .flatMap {
+                   case SupportedFileType.StillImage => handleImageFile(original, assetRef)
+                   case SupportedFileType.Other      => handleOtherFile(original, assetRef, assetDir)
+                   case SupportedFileType.MovingImage =>
+                     ZIO.fail(new NotImplementedError("Video files are not supported yet."))
                  }
       _ <- assetInfo.createAssetInfo(asset)
       _ <- storage.delete(fileToIngest)
       _ <- ZIO.logInfo(s"Finished ingesting file $fileToIngest")
     } yield asset
 
-  private def createOriginalFileInAssetDir(file: Path, assetRef: AssetRef): IO[IOException, Original] = for {
-    _               <- ZIO.logInfo(s"Creating original for $file, $assetRef")
-    assetDir        <- storage.getAssetDirectory(assetRef)
-    originalPath     = assetDir / s"${assetRef.id}.${file.fileExtension}.orig"
-    _               <- storage.copyFile(file, originalPath)
-    originalFile     = OriginalFile.unsafeFrom(originalPath)
-    originalFileName = NonEmptyString.unsafeFrom(file.filename.toString)
-  } yield Original(originalFile, originalFileName)
+  private def ensureAssetDirectoryExists(assetRef: AssetRef): IO[IOException, Path] =
+    storage.getAssetDirectory(assetRef).tap(storage.createDirectories(_))
+
+  private def createOriginalFileInAssetDir(file: Path, assetRef: AssetRef, assetDir: Path): IO[IOException, Original] =
+    for {
+      _               <- ZIO.logInfo(s"Creating original for $file, $assetRef")
+      originalPath     = assetDir / s"${assetRef.id}.${file.fileExtension}.orig"
+      _               <- storage.copyFile(file, originalPath)
+      originalFile     = OriginalFile.unsafeFrom(originalPath)
+      originalFileName = NonEmptyString.unsafeFrom(file.filename.toString)
+    } yield Original(originalFile, originalFileName)
 
   private def handleImageFile(original: Original, assetRef: AssetRef): Task[StillImageAsset] =
     ZIO.logInfo(s"Creating derivative for image $original, $assetRef") *>
@@ -64,17 +62,14 @@ final case class IngestService(
         .createDerivative(original.file)
         .map(derivative => Asset.makeStillImage(assetRef, original, derivative))
 
-  private def handleOtherFile(original: Original, assetRef: AssetRef): Task[OtherAsset] = {
-    def createDerivative(original: Original) =
-      for {
-        folder        <- storage.getAssetDirectory(assetRef)
-        derivativePath = folder / s"${assetRef.id}.${original.file.toPath.fileExtension}"
-        _             <- storage.copyFile(original.file.toPath, derivativePath)
-      } yield OtherDerivativeFile.unsafeFrom(derivativePath)
-
-    ZIO.logInfo(s"Creating derivative for other $original, $assetRef") *>
-      createDerivative(original).map(derivative => Asset.makeOther(assetRef, original, derivative))
-  }
+  private def handleOtherFile(original: Original, assetRef: AssetRef, assetDir: Path): Task[OtherAsset] =
+    ZIO.logInfo(s"Creating derivative for other $original, $assetRef") *> {
+      val derivativePath = assetDir / s"${assetRef.id}.${original.file.toPath.fileExtension}"
+      storage
+        .copyFile(original.file.toPath, derivativePath)
+        .as(OtherDerivativeFile.unsafeFrom(derivativePath))
+        .map(derivative => Asset.makeOther(assetRef, original, derivative))
+    }
 }
 
 object IngestService {
