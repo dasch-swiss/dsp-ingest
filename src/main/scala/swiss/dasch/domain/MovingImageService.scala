4d3cd3cbaa5de066efde40905ba4acba5c1b443f
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2023-12-07 17:53:42 +0100

    extract executor

--- src/main/scala/swiss/dasch/domain/MovingImageService.scala
+++ src/main/scala/swiss/dasch/domain/MovingImageService.scala
@@ -11,7 +11,7 @@ import scala.sys.process.{ProcessLogger, stringToProcess}
 
 final case class MovingImageMetadata(width: Int, height: Int, duration: Double, fps: Int)
 
-case class MovingImageService(storage: StorageService, config: SipiConfig) {
+case class MovingImageService(storage: StorageService, config: SipiConfig, executor: CommandExecutor) {
 
   def createDerivative(original: Original, assetRef: AssetRef): Task[MovingImageDerivativeFile] = {
     val fileExtension = FilenameUtils.getExtension(original.originalFilename.toString)
@@ -36,40 +36,14 @@ case class MovingImageService(storage: StorageService, config: SipiConfig) {
   private def runInDocker(entrypoint: String, params: String, assetPath: Path) =
     s"docker run --entrypoint $entrypoint -v $assetPath:$assetPath daschswiss/knora-sipi:latest $params"
 
-  final private class ProcessOutput(val out: String, val err: String)
-
-  final private class InMemoryProcessLogger extends ProcessLogger {
-    private val sbOut = new StringBuilder
-    private val sbErr = new StringBuilder
-
-    override def out(s: => String): Unit = sbOut.append(s + "\n")
-
-    override def err(s: => String): Unit = sbErr.append(s + "\n")
-
-    override def buffer[T](f: => T): T = f
-
-    def getOutput: ProcessOutput = ProcessOutput(sbOut.toString(), sbErr.toString())
-  }
-
   def extractMetadata(file: DerivativeFile, assetRef: AssetRef): Task[MovingImageMetadata] =
     for {
       assetDir <- storage.getAssetDirectory(assetRef).flatMap(_.parent.head.toAbsolutePath)
       absPath  <- file.toPath.toAbsolutePath
       command = if (config.useLocalDev) { runInDocker(extractCommand, extractFlags + " " + absPath, assetDir) }
                 else { s"$extractCommand $extractFlags $absPath" }
-      metadata <- executeCommand(command).flatMap(parseMetadata)
+      metadata <- executor.execute(command).flatMap(parseMetadata)
     } yield metadata
-
-  private def executeCommand(command: String) = {
-    val logger = InMemoryProcessLogger()
-    for {
-      _   <- ZIO.logInfo(s"Executing command: $command")
-      out <- ZIO.attemptBlockingIO(command ! logger).as(logger.getOutput)
-      _ <- ZIO.when(out.err.nonEmpty)(
-             ZIO.fail(new RuntimeException(s"Failed executing '$command' with error '${out.err}''"))
-           )
-    } yield out.out
-  }
 
   final case class FfprobeStream(width: Int, height: Int, duration: Double, r_frame_rate: String)
   object FfprobeStream {
@@ -95,5 +69,37 @@ case class MovingImageService(storage: StorageService, config: SipiConfig) {
 }
 
 object MovingImageService {
-  val layer = ZLayer.derive[MovingImageService]
+  val layer = CommandExecutor.layer >>> ZLayer.derive[MovingImageService]
+}
+
+final case class CommandExecutor() {
+  private case class ProcessOutput(val out: String, val err: String)
+
+  private class InMemoryProcessLogger extends ProcessLogger {
+    private val sbOut = new StringBuilder
+    private val sbErr = new StringBuilder
+
+    override def out(s: => String): Unit = sbOut.append(s + "\n")
+
+    override def err(s: => String): Unit = sbErr.append(s + "\n")
+
+    override def buffer[T](f: => T): T = f
+
+    def getOutput: ProcessOutput = ProcessOutput(sbOut.toString(), sbErr.toString())
+  }
+
+  def execute(command: String): Task[String] = {
+    val logger = new InMemoryProcessLogger()
+    for {
+      _   <- ZIO.logInfo(s"Executing command: $command")
+      out <- ZIO.attemptBlockingIO(command ! logger).as(logger.getOutput)
+      _ <- ZIO.when(out.err.nonEmpty)(
+             ZIO.fail(new RuntimeException(s"Failed executing '$command' with error '${out.err}''"))
+           )
+    } yield out.out
+  }
+}
+
+object CommandExecutor {
+  val layer = ZLayer.derive[CommandExecutor]
 }
