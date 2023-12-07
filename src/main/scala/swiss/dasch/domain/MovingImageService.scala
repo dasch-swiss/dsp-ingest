f9ab83818dd2d86ef02f9f426e30bcbce2876813
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2023-12-07 18:12:31 +0100

    add build command to exectutor

--- src/main/scala/swiss/dasch/domain/MovingImageService.scala
+++ src/main/scala/swiss/dasch/domain/MovingImageService.scala
@@ -4,7 +4,6 @@ import org.apache.commons.io.FilenameUtils
 import swiss.dasch.config.Configuration.SipiConfig
 import swiss.dasch.domain.DerivativeFile.MovingImageDerivativeFile
 import zio.json.{DecoderOps, DeriveJsonDecoder, JsonDecoder}
-import zio.nio.file.Path
 import zio.{Task, ZIO, ZLayer}
 
 import scala.sys.process.{ProcessLogger, stringToProcess}
@@ -29,20 +28,15 @@ case class MovingImageService(storage: StorageService, config: SipiConfig, execu
   def extractKeyFrames(file: DerivativeFile, assetRef: AssetRef): Task[Unit] =
     ZIO.logInfo(s"Extracting key frames for $file, $assetRef")
 
-  private val extractCommand = "ffprobe"
-  private val extractFlags =
-    "-v error -select_streams v:0 -show_entries stream=width,height,duration,r_frame_rate -print_format json -i"
-
-  private def runInDocker(entrypoint: String, params: String, assetPath: Path) =
-    s"docker run --entrypoint $entrypoint -v $assetPath:$assetPath daschswiss/knora-sipi:latest $params"
-
   def extractMetadata(file: DerivativeFile, assetRef: AssetRef): Task[MovingImageMetadata] =
     for {
-      assetDir <- storage.getAssetDirectory(assetRef).flatMap(_.parent.head.toAbsolutePath)
-      absPath  <- file.toPath.toAbsolutePath
-      command = if (config.useLocalDev) { runInDocker(extractCommand, extractFlags + " " + absPath, assetDir) }
-                else { s"$extractCommand $extractFlags $absPath" }
-      metadata <- executor.execute(command).flatMap(parseMetadata)
+      absPath <- file.toPath.toAbsolutePath
+      cmd <-
+        executor.buildCommand(
+          "ffprobe",
+          s"-v error -select_streams v:0 -show_entries stream=width,height,duration,r_frame_rate -print_format json -i $absPath"
+        )
+      metadata <- executor.execute(cmd).flatMap(parseMetadata)
     } yield metadata
 
   final case class FfprobeStream(width: Int, height: Int, duration: Double, r_frame_rate: String)
@@ -72,7 +66,19 @@ object MovingImageService {
   val layer = CommandExecutor.layer >>> ZLayer.derive[MovingImageService]
 }
 
-final case class CommandExecutor() {
+final case class Cmd(cmd: String)
+final case class CommandExecutor(sipiConfig: SipiConfig, storageService: StorageService) {
+
+  def buildCommand(command: String, params: String): Task[Cmd] =
+    for {
+      assetDir <- storageService.getAssetDirectory().flatMap(_.toAbsolutePath)
+    } yield
+      if (sipiConfig.useLocalDev) {
+        Cmd(s"docker run --entrypoint $command -v $assetDir:$assetDir daschswiss/knora-sipi:latest $params")
+      } else {
+        Cmd(s"$command $params")
+      }
+
   private case class ProcessOutput(out: String, err: String)
 
   private class InMemoryProcessLogger extends ProcessLogger {
@@ -88,13 +94,13 @@ final case class CommandExecutor() {
     def getOutput: ProcessOutput = ProcessOutput(sbOut.toString(), sbErr.toString())
   }
 
-  def execute(command: String): Task[String] = {
+  def execute(command: Cmd): Task[String] = {
     val logger = new InMemoryProcessLogger()
     for {
-      _   <- ZIO.logInfo(s"Executing command: $command")
-      out <- ZIO.attemptBlockingIO(command ! logger).as(logger.getOutput)
+      _   <- ZIO.logInfo(s"Executing command: ${command.cmd}")
+      out <- ZIO.attemptBlockingIO(command.cmd ! logger).as(logger.getOutput)
       _ <- ZIO.when(out.err.nonEmpty)(
-             ZIO.fail(new RuntimeException(s"Failed executing '$command' with error '${out.err}''"))
+             ZIO.fail(new RuntimeException(s"Failed executing '${command.cmd}' with error '${out.err}''"))
            )
     } yield out.out
   }
