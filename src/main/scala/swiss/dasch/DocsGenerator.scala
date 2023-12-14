4e0e6c6552a5b78298d145ded1d1fad5e8cc7bfd
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2023-12-14 20:01:01 +0100

    add generator for all other endpoints

--- src/main/scala/swiss/dasch/DocsGenerator.scala
+++ src/main/scala/swiss/dasch/DocsGenerator.scala
@@ -1,10 +1,10 @@
 package swiss.dasch
 
-import sttp.apispec.openapi.circe.yaml._
-import sttp.tapir.AnyEndpoint
+import sttp.apispec.openapi.circe.yaml.*
 import sttp.tapir.docs.openapi.OpenAPIDocsInterpreter
 import swiss.dasch.api.*
 import swiss.dasch.config.Configuration
+import swiss.dasch.version.BuildInfo
 import zio.{ZIO, ZIOAppDefault}
 
 object DocsGenerator extends ZIOAppDefault {
@@ -12,13 +12,13 @@ object DocsGenerator extends ZIOAppDefault {
   override def run = {
     val interp = OpenAPIDocsInterpreter()
     for {
-      _                      <- ZIO.logInfo("Generating OpenAPI docs")
-      mon: List[AnyEndpoint] <- ZIO.serviceWith[MonitoringEndpoints](_.endpoints)
-      prj                    <- ZIO.serviceWith[ProjectsEndpoints](_.endpoints)
-      mtn                    <- ZIO.serviceWith[MaintenanceEndpoints](_.endpoints)
-      //      apis: Seq[AnyEndpoint]  = mon ++ prj ++ mtn
-      docs                   =interp.toOpenAPI(mon, "SwissDasch", "1.0.0")
-      _                      <- ZIO.logInfo(s"Found ${docs.toYaml}")
+      _   <- ZIO.logInfo("Generating OpenAPI docs")
+      mon <- ZIO.serviceWith[MonitoringEndpoints](_.endpoints)
+      prj <- ZIO.serviceWith[ProjectsEndpoints](_.endpoints)
+      mtn <- ZIO.serviceWith[MaintenanceEndpoints](_.endpoints)
+      apis = mon ++ (prj ++ mtn).map(_.endpoint)
+      docs = interp.toOpenAPI(apis, BuildInfo.name, BuildInfo.version)
+      _   <- ZIO.logInfo(s"Found ${docs.toYaml}")
     } yield 0
   }.provide(
     AuthServiceLive.layer,
