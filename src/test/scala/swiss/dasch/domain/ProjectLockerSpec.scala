6b52fdf3e1bb61b5a004fcac1fa0426265ad6ab2
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-06-21 13:33:38 +0200

    add test for failed tasks

--- src/test/scala/swiss/dasch/domain/ProjectLockerSpec.scala
+++ src/test/scala/swiss/dasch/domain/ProjectLockerSpec.scala
@@ -1,3 +1,8 @@
+/*
+ * Copyright © 2021 - 2024 Swiss National Data and Service Center for the Humanities and/or DaSCH Service Platform contributors.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package swiss.dasch.domain
 
 import swiss.dasch.domain.ProjectLocker.ProjectLocked
@@ -11,19 +16,20 @@ final case class ProjectLocker(private val semaphoresPerProject: TMap[ProjectSho
     semaphore <- semaphoresPerProject.getOrElseSTM(key, TSemaphore.make(1))
     _         <- semaphoresPerProject.put(key, semaphore)
     _         <- semaphore.acquire
-  } yield semaphore).commit.disconnect
+  } yield semaphore).commit
     .timeout(ProjectLocker.acquireTimeOut)
     .some
     .orElseFail(ProjectLocked(key))
 
-  def withSemaphore[E, A](key: ProjectShortcode)(
+  def withSemaphore[E, A](key: ProjectShortcode)(zio: IO[E, A]): ZIO[Any, ProjectLocked | E, A] =
+    getLock(key).flatMap(semaphore => zio.logError.ensuring(semaphore.release.commit))
+
+  def withSemaphoreForkDaemon[E, A](key: ProjectShortcode)(
     zio: IO[E, A],
-  ): ZIO[Any, ProjectLocked | E, A] =
-    for {
-      semaphore <- getLock(key)
-      result    <- zio.logError.ensuring(semaphore.release.commit)
-    } yield result
+  ): ZIO[Any, ProjectLocked, Fiber.Runtime[E, A]] =
+    getLock(key).flatMap(semaphore => zio.logError.ensuring(semaphore.release.commit).forkDaemon)
 }
+
 object ProjectLocker {
   final case class ProjectLocked(shortcode: ProjectShortcode)
   val acquireTimeOut: Duration = 2.seconds
@@ -32,38 +38,73 @@ object ProjectLocker {
 
 object ProjectLockerSpec extends ZIOSpecDefault {
 
-  private val shortcode: ProjectShortcode                  = ProjectShortcode.unsafeFrom("0001")
-  private val lock                                         = ZIO.serviceWithZIO[ProjectLocker]
-  private def attemptRunWithSemaphore(zio: UIO[Int])       = lock(_.withSemaphore(shortcode)(zio))
+  private val shortcode: ProjectShortcode       = ProjectShortcode.unsafeFrom("0001")
+  private val lock                              = ZIO.serviceWithZIO[ProjectLocker]
+  private def withSemaphore[E](zio: IO[E, Int]) = lock(_.withSemaphore(shortcode)(zio))
+//  private def withSemaphoreForkDaemon(zio: UIO[Int])       = lock(_.withSemaphoreForkDaemon(shortcode)(zio))
   private def slowSuccessTask(nr: Int, duration: Duration) = ZIO.succeed(nr).delay(duration)
+  private def slowFailedTask(nr: Int, duration: Duration)  = ZIO.fail(nr).delay(duration)
 
   val spec = suite("ProjectLocker")(
-    test("should run a success task") {
-      for {
-        fork <- lock(_.withSemaphore(shortcode)(slowSuccessTask(1, 1.second))).fork
-        _    <- TestClock.adjust(1.second)
-        exit <- fork.join
-      } yield assertTrue(exit == 1)
-    },
-    test("should prevent another task to run if lock is not acquired within acquireTimeout") {
-      for {
-        fork1   <- attemptRunWithSemaphore(slowSuccessTask(1, 3.seconds)).fork
-        _       <- TestClock.adjust(1.second) // wait 1 seconds so that task cannot acquire lock before the other is done
-        fork2   <- attemptRunWithSemaphore(slowSuccessTask(2, 3.seconds)).fork
-        _       <- TestClock.adjust(10.seconds)
-        result1 <- fork1.join
-        exit2   <- fork2.join.exit
-      } yield assertTrue(result1 == 1, exit2.isFailure)
-    },
-    test("should allow another task to run if lock is acquired within acquireTimeout") {
-      for {
-        fork1   <- attemptRunWithSemaphore(slowSuccessTask(1, 3.seconds)).fork
-        _       <- TestClock.adjust(2.seconds) // wait 2 seconds so that task can acquire lock after the other is done
-        fork2   <- attemptRunWithSemaphore(slowSuccessTask(2, 3.seconds)).fork
-        _       <- TestClock.adjust(10.seconds)
-        result1 <- fork1.join
-        result2 <- fork2.join
-      } yield assertTrue(result1 == 1, result2 == 2)
-    },
+    suite("withSemaphore")(
+      suite("with success task")(
+        test("should run a success task") {
+          for {
+            fork <- lock(_.withSemaphore(shortcode)(slowSuccessTask(1, 1.second))).fork
+            _    <- TestClock.adjust(1.second)
+            exit <- fork.join
+          } yield assertTrue(exit == 1)
+        },
+        test("should prevent another task to run if lock is not acquired within acquireTimeout") {
+          for {
+            fork1   <- withSemaphore(slowSuccessTask(1, 3.seconds)).fork
+            _       <- TestClock.adjust(1.second) // wait 1 seconds so that task cannot acquire lock before the other is done
+            fork2   <- withSemaphore(slowSuccessTask(2, 3.seconds)).fork
+            _       <- TestClock.adjust(10.seconds)
+            result1 <- fork1.join
+            exit2   <- fork2.join.exit
+          } yield assertTrue(result1 == 1, exit2 == Exit.fail(ProjectLocked(shortcode)))
+        },
+        test("should allow another task to run if lock is acquired within acquireTimeout") {
+          for {
+            fork1   <- withSemaphore(slowSuccessTask(1, 3.seconds)).fork
+            _       <- TestClock.adjust(2.seconds) // wait 2 seconds so that task can acquire lock after the other is done
+            fork2   <- withSemaphore(slowSuccessTask(2, 3.seconds)).fork
+            _       <- TestClock.adjust(10.seconds)
+            result1 <- fork1.join
+            result2 <- fork2.join
+          } yield assertTrue(result1 == 1, result2 == 2)
+        },
+      ),
+      suite("with failed task")(
+        test("should run a success task") {
+          for {
+            fork <- lock(_.withSemaphore(shortcode)(slowFailedTask(1, 1.second))).fork
+            _    <- TestClock.adjust(1.second)
+            exit <- fork.join.exit
+          } yield assertTrue(exit == Exit.fail(1))
+        },
+        test("should prevent another task to run if lock is not acquired within acquireTimeout") {
+          for {
+            fork1 <- withSemaphore(slowFailedTask(1, 3.seconds)).fork
+            _     <- TestClock.adjust(1.second) // wait 1 seconds so that task cannot acquire lock before the other is done
+            fork2 <- withSemaphore(slowFailedTask(2, 3.seconds)).fork
+            _     <- TestClock.adjust(10.seconds)
+            exit1 <- fork1.join.exit
+            exit2 <- fork2.join.exit
+          } yield assertTrue(exit1 == Exit.fail(1), exit2 == Exit.fail(ProjectLocked(shortcode)))
+        },
+        test("should allow another task to run if lock is acquired within acquireTimeout") {
+          for {
+            fork1 <- withSemaphore(slowFailedTask(1, 3.seconds)).fork
+            _     <- TestClock.adjust(2.seconds) // wait 2 seconds so that task can acquire lock after the other is done
+            fork2 <- withSemaphore(slowFailedTask(2, 3.seconds)).fork
+            _     <- TestClock.adjust(10.seconds)
+            exit1 <- fork1.join.exit
+            exit2 <- fork2.join.exit
+          } yield assertTrue(exit1 == Exit.fail(1), exit2 == Exit.fail(2))
+        },
+      ),
+    ),
   ).provide(ProjectLocker.layer)
 }
